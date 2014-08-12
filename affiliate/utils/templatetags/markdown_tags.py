84960ac0beb11ae86c48d1e7fc5c27cc4c1f93e4
Stuart <stuart.leigh83@gmail.com>
2014-08-12 17:30:56 +0100

    adding twitter cards to product listing pages

--- affiliate/utils/templatetags/markdown_tags.py
+++ affiliate/utils/templatetags/markdown_tags.py
@@ -1,4 +1,5 @@
 import markdown as Markdown
+import re
 
 from django import template
 from django.utils.safestring import mark_safe
@@ -6,7 +7,18 @@ from django.utils.safestring import mark_safe
  
 register = template.Library()
 
+
+SUMMARY_RE = re.compile("(.*?)(\\r\\n|\\n)")
  
 @register.filter
 def markdown(text):
 	return mark_safe(Markdown.markdown(text, safe_mode='escape'))
+
+
+@register.filter
+def summary(text):
+	match = SUMMARY_RE.match(text)
+	try:
+		return match.groups()[0]
+	except AttributeError:
+		return text
