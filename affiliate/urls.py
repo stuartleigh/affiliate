f1e32d29f8307872347576da3b362be92a8bbc0e
Stuart <stuart.leigh83@gmail.com>
2014-08-12 21:00:05 +0100

    adding custom 404 and 500 pages

--- affiliate/urls.py
+++ affiliate/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+handler404 = 'affiliate.views.custom_404'
+handler404 = 'affiliate.views.custom_500'
+
 urlpatterns = patterns('',
     url(r'^$', 'affiliate.views.home', name='home'),
     url(r'^p/', include('affiliate.product.urls')),
--- affiliate/views.py
+++ affiliate/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.shortcuts import render
 
 
@@ -17,3 +19,16 @@ def sitemap(request):
 	products = request.site.products.all()
 	context = {"products": products}
 	return render(request, "sitemap.xml", context, content_type="application/xhtml+xml")
+
+
+def custom_404(request):
+	count = request.site.products.count()
+	random_num = random.choice(range(1, count))
+	random_product = request.site.products.all()[random_num - 1]
+	return render(request, '404.html', {"product": random_product})
+
+
+def custom_500(request):
+	count = request.site.products.count()
+	random_num = random.choice(range(1, count))
+	return render(request, '500.html')
